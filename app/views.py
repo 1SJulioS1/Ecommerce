83b06470e109ef0c504a09f11436023e0fe2339e
1SJulioS1 <91168844+1SJulioS1@users.noreply.github.com>
2023-06-25 18:24:14 -0500

    fixed bugs in user management

--- app/views.py
+++ app/views.py
@@ -79,7 +79,7 @@ class UserLogoutView(APIView):
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
-            return Response({"message":"Logout succeeded"}status=status.HTTP_205_RESET_CONTENT)
+            return Response({"message": "Logout succeeded"}, status=status.HTTP_205_RESET_CONTENT)
         except Exception as e:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
